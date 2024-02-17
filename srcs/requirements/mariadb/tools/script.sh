97b8fb00efa80bf95880784865257c0a9a9e939a
Emily Sal <emily.sali@outlook.com>
2024-02-17 13:27:34 -0600

    update nginx and mariadb, add Makefile

---
+++ srcs/requirements/mariadb/tools/script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+service mysql start;
+
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+
+mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+
+mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+
+#modify root user
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+
+#refresh privileges
+mysql -e "FLUSH PRIVILEGES;"
+
+#restart MYSQL
+mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+mysqld_safe
