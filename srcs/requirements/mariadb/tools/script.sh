7201249ba507a9ae2185ec17dea313189aaadc5b
Emily Sali <emily.sali@outlook.com>
2024-02-18 08:55:15 -0600

    update mariadb to debian:bullseye

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,19 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 
-service mysql start;
+service mariadb start;
 
 mysql -e "CREATE DATABASE IF NOT EXISTS Test_DB;"
 
-mysql -e "CREATE USER IF NOT EXISTS `esali`@'localhost' IDENTIFIED BY '12345';"
+mysql -e "CREATE USER IF NOT EXISTS 'esali'@'%' IDENTIFIED BY '12345';"
 
-mysql -e "GRANT ALL PRIVILEGES ON Test_DB.* TO `esali`@'%' IDENTIFIED BY '12345';"
+mysql -e "GRANT ALL PRIVILEGES ON Test_DB.* TO 'esali'@'%' IDENTIFIED BY '12345';"
 
 #modify root user
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345';"
+mysql -u root --skip-password  -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345';"
 
 #refresh privileges
-mysql -e "FLUSH PRIVILEGES;"
+mysql  -e "FLUSH PRIVILEGES;"
 
 #restart MYSQL
-mysqladmin -u root -p '12345'  shutdown
+kill $(cat /var/run/mysqld/mysqld.pid)
 mysqld_safe
