64ef4397efd4d7a85f9ef7466e68a42618caa140
Emily Sali <emily.sali@outlook.com>
2024-02-19 05:54:48 -0600

    setup mariadb with .sql init file

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,19 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
-service mariadb start;
+service mariadb start
 
-mysql -e "CREATE DATABASE IF NOT EXISTS Test_DB;"
+mysql -e "CREATE DATABASE IF NOT EXISTS mariadb;"
 
 mysql -e "CREATE USER IF NOT EXISTS 'esali'@'%' IDENTIFIED BY '12345';"
 
-mysql -e "GRANT ALL PRIVILEGES ON Test_DB.* TO 'esali'@'%' IDENTIFIED BY '12345';"
+mysql -e "GRANT ALL PRIVILEGES ON mariadb.* TO 'esali'@'%';"
 
-#modify root user
-mysql -u root --skip-password  -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345';"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root12345';"
 
 #refresh privileges
-mysql  -e "FLUSH PRIVILEGES;"
+mysql -e "FLUSH PRIVILEGES:"
+
+#modify root user
+mysql -u root --skip-password  -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root12345';"
 
 #restart MYSQL
-kill $(cat /var/run/mysqld/mysqld.pid)
-mysqld_safe
+#kill $(cat /var/run/mysqld/mysqld.pid)
+mysqladmin -u root -p12345 shutdown
+exec "mysqld_safe"
